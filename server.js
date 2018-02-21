f6d17ae98ede75f9f222bab0a9d3b8471f2914ef
katasanirohith <33127885+katasanirohith@users.noreply.github.com>
2018-02-21 22:11:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/king', function (req, res) {
+  res.sendFile("This thing is working!");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
