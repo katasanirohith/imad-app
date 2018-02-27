4bd019afc0d51d1e76c610886954954c336371a5
katasanirohith <33127885+katasanirohith@users.noreply.github.com>
2018-02-27 19:00:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,9 @@
 console.log('Loaded!');
 console.log('Yes!');
+var element= document.getElementById('maintext');
+element.innerHTML='Under construction';
 var img= document.getElementById('madi');
-img.onclick = function(){
+/*img.onclick = function(){
     img.style.marginleft='1000px';
     
-};
+};*/
