7246165e20ba388f58ac2c4cb9dddfc6246c4bd8
katasanirohith <33127885+katasanirohith@users.noreply.github.com>
2018-02-27 19:01:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,9 @@ console.log('Loaded!');
 console.log('Yes!');
 console.log('Yes!');
 var element= document.getElementById('maintext');
-element.innerHTML='Under construction';
+element.innerHTML='This website is under Construction By King. Please wait for few more days';
 var img= document.getElementById('madi');
-/*img.onclick = function(){
+img.onclick = function(){
     img.style.marginleft='1000px';
     
-};*/
+};
