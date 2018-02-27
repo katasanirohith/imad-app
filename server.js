150dd1d42a36344bfbbdc68eb1316c82c8024a93
katasanirohith <33127885+katasanirohith@users.noreply.github.com>
2018-02-27 18:31:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ res.sendFile(path.join(__dirname, 'ui', 'IMG_20170709_095419_216__1503223529_14.
 });
 
 app.get('/rohith', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'javascript1.html'));
+res.sendFile(path.join(__dirname, 'ui', 'javascript_1.html'));
 });
 
 app.get('/article-one', function (req, res) {
